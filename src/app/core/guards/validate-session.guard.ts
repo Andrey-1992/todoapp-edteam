092fad26c6d9e902dd05818f46dafe88798d4c0b
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-02-27 20:47:10 -0700

    setup-logic-for-checking-cookie

--- src/app/core/guards/validate-session.guard.ts
+++ src/app/core/guards/validate-session.guard.ts
@@ -7,12 +7,22 @@ import { Observable } from 'rxjs';
 })
 export class ValidateSessionGuard implements CanActivate {
 
-  private cookie:string | null = '' // We would consult to the browser
+  private cookie:string | null = null; // We would consult to the browser
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
+  }
+
+  private checkCookie():boolean{
+    //return (this.cookie !== null). --> Fancier way to write it
+
+    if (this.cookie !== null) {
+      return true;
+    } else {
+      return true;
+    }
   }
   
 }
