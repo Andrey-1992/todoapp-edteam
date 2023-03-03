4d04b5439ae89a38e8c90ffaef1b61f386954e8f
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-03 11:41:00 -0700

    fix-connection-with-routes-within-loginpage-component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './page/login-page.component';
 import { SharedModule } from '@shared/shared.module';
