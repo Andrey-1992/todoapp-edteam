d52d0bbafe6633689ae6d86c1d0e2950ec9764c8
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-03 17:06:06 -0700

    add-max-length-and-mion-for-poassweord

--- src/app/modules/auth/page/login-page.component.ts
+++ src/app/modules/auth/page/login-page.component.ts
@@ -19,7 +19,11 @@ export class LoginPageComponent {
     this.loginForm = new FormGroup(
       {
         email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required])
+        password: new FormControl('', [
+          Validators.required, 
+          Validators.minLength(5),
+          Validators.maxLength(12)
+        ])
       }
     )
 
