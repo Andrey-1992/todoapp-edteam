af33ad1a5c0c7c325981942a929697ff43c46bbc
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-09 14:56:22 -0700

    create-action-for-validators

--- src/app/modules/auth/services/auth-test.service.ts
+++ src/app/modules/auth/services/auth-test.service.ts
@@ -1,9 +1,30 @@
+// import { environment } from './../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
+import { catchError, delay, map, Observable, of, tap } from 'rxjs';
+// import { CookieService } from 'ngx-cookie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthTestService {
 
-  constructor() { }
+  constructor() { 
+    uniqueEmailValidator(): AsyncValidatorFn {
+      return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      //   return of(control.value).pipe(
+      //     delay(500),
+      //     map((email) => {
+      //       const emails = ['test@test.com', 'hola@gmail.com'];
+      //       return emails.includes(email);
+      //     })
+      //   ).pipe(
+      //     map((exists) => (exists ? { emailExists: true } : null)),
+      //     catchError(async () => null)
+      //   );
+      };
+    }
+
+  }
 }
