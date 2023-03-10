7201bbae02a75c774575494c84500551607b5b05
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-10 09:09:45 -0700

    create-change-functio-to-hise-or-show

--- src/app/modules/task/page/task-page.component.ts
+++ src/app/modules/task/page/task-page.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-page.component.css']
 })
 export class TaskPageComponent {
+  showText = true;
+
+  ngOnInti(): void {
+
+  }
+
+  changeText():void {
+    this.showText = !this.showText;
+  }
 
 }
