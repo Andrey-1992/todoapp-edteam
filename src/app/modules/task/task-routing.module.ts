b659ce1829d1ebc788644d3a8d3fa5c07273aacd
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-09 17:37:12 -0700

    reate-route-for-header-component

--- src/app/modules/task/task-routing.module.ts
+++ src/app/modules/task/task-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 import { TaskPageComponent } from './page/task-page.component';
 
 //http:localhost:4200/task/
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path:'',
     component: TaskPageComponent
+  },
+  {
+    path:'header',
+    component: HeaderComponent
   }
 ];
 
