ffdff21fd26abb6baaf0a23e25d1218172d579e3
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-11 13:30:10 -0700

    set-up-card-component-typescript-loigc

--- src/app/modules/task/components/card/card.component.ts
+++ src/app/modules/task/components/card/card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, Component, DoCheck, ElementRef, Input, OnChanges, OnInit, Renderer2, SimpleChanges, ViewChild } from '@angular/core';
+// import { TestCourseService } from '@shared/services/test-course.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  // changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class CardComponent {
+export class CardComponent  {
+  @ViewChild('refId') elementRefId:ElementRef = new ElementRef('hola')
+  @Input() idOrder: string | number = 0
+  @Input() items: Array<any> = []
 
+  constructor(
+    // private testCourseService:TestCourseService
+    ) { }
+
+
+  sendData():void{
+    // this.testCourseService.setData('Hola desde card 😉')
+  }
+  
 }
