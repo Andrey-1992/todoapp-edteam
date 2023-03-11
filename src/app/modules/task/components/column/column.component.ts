b05aabb1cc1a0d46bedee9176eba434587c0eb4f
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-11 11:42:50 -0700

    add-html-code-to-task-opage-component

--- src/app/modules/task/components/column/column.component.ts
+++ src/app/modules/task/components/column/column.component.ts
@@ -1,13 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-column',
   templateUrl: './column.component.html',
   styleUrls: ['./column.component.css']
 })
-export class ColumnComponent implements OnInit{
+export class ColumnComponent implements OnInit, OnChanges{
   
   @Input() data:Array<{id:string, name:string}> = [];
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('---->', changes)
+  }
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
