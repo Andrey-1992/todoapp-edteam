03dc91ad380ccb28364e973df1c88009049a8c89
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-10 18:25:14 -0700

    implement-onInit

--- src/app/modules/task/components/column/column.component.ts
+++ src/app/modules/task/components/column/column.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-column',
   templateUrl: './column.component.html',
   styleUrls: ['./column.component.css']
 })
-export class ColumnComponent {
+export class ColumnComponent implements OnInit{
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
