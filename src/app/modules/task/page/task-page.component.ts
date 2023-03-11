71238f97392b432901e72635e619ae3e061428e0
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-11 11:48:28 -0700

    create-mock-up-data

--- src/app/modules/task/page/task-page.component.ts
+++ src/app/modules/task/page/task-page.component.ts
@@ -7,15 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskPageComponent implements OnInit {
 
-  showText = true;
-  case:number = 0;
+  // showText = true;
+  // case:number = 0;
+  // listUser:Array<{id:string, name:string}> = [];
+  groups:Array<any> = [];
 
-  listUser:Array<{id:string, name:string}> = [
-
-  ];
-  
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.groups = [
+      {
+        label: "Nuevos",
+        color: "tomato",
+        list: [
+          {
+            order: "Como instalar Angular",
+            items: [
+              {
+                key: "price",
+                value: 152
+              },
+              {
+                key: "time",
+                value: 152
+              }
+            ]
+          }
+        ]
+      },
+      {
+        label: "Trabajando",
+        color: "green",
+        list: [
+          {
+            order: "Como instalar Angular",
+            items: [
+              {
+                key: "price",
+                value: 152
+              },
+              {
+                key: "time",
+                value: 152
+              }
+            ]
+          }
+        ]
+      },
+    ]
+
+    // this.taskService.getTask()
+    // .subscribe((response) => {
+
+    //   const {data} = response;
+    //   this.groups = data;
+    //   console.log('--->',response)
+
+    // })
+
   }
 
   // changeText():void {
