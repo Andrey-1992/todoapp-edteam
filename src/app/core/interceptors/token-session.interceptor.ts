6f36569a43de23a89323f5981ea5667981908fcd
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-03-14 21:48:07 -0600

    create-interceptor-logic

--- src/app/core/interceptors/token-session.interceptor.ts
+++ src/app/core/interceptors/token-session.interceptor.ts
@@ -6,13 +6,25 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie';
 
 @Injectable()
 export class TokenSessionInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private cookieService:CookieService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = this.cookieService.get('token_session');
+    const currentRequest = request.clone(
+      {
+        setHeaders:{
+          authorization: `Bearer ${token}`
+        }
+      }
+    )
+
+    request = currentRequest;
+
     return next.handle(request);
   }
 }
